43ac0792c8e6c4a8302c42dd22b8279009a1cc87
ddobagi <freshcom@snu.ac.kr>
2025-03-11 04:00:29 +0900

    console.log(currentUser.email)

--- app/dashboard/[firstSlug]/page.js
+++ app/dashboard/[firstSlug]/page.js
@@ -69,6 +69,7 @@ export default function VideoDetail() {
         if (currentUser) {
             setUser(currentUser);
             setUserEmail(currentUser.email);
+            console.log(currentUser.email);
             console.log(firstSlug);
 
             try {
