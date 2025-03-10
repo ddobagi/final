65a6d3538b7421010b2d9e2382b5b604054e6d2c
ddobagi <freshcom@snu.ac.kr>
2025-03-10 17:44:31 +0900

    유튜브 프로필 이미지 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ["yt3.ggpht.com"], // 유튜브 프로필 이미지 도메인 추가
+    },
+  };
+  
+  export default nextConfig;
+  
