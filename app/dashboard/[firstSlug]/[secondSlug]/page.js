61cd3687f3505eeef14e59c9fe3408e13f611b90
ddobagi <freshcom@snu.ac.kr>
2025-03-10 20:43:32 +0900

    콘솔 수정

--- app/dashboard/[firstSlug]/[secondSlug]/page.js
+++ app/dashboard/[firstSlug]/[secondSlug]/page.js
@@ -17,7 +17,6 @@ export default function SecondSlugPage() {
 
   // useRouter(): 페이지 이동을 관리하는 hook 
   const router = useRouter();
-  console.log(router.pathname)
 
   // useState() : react에서 상태를 관리하는 hook 
   const [user, setUser] = useState(null);
